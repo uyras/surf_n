a025507c13b72d5d01e350ac7224afe2897692b5
uyras <150030@mail.ru>
2012-11-14 07:24:50 +1000

    основнй каркас, осталось вписать формулу

--- main.cpp
+++ main.cpp
@@ -1,57 +1,171 @@
 #include<iostream>
 #include <stdlib.h>
+#include <vector>
+#include <math.h>
+
 using namespace std;
 
+int N = 6, total = N * N*N; //count of particles
+double
+I0s = 500, //намагниченность насыщения в гаусс,
+		hc = 50, //критическое поле в эрстедах,
+		vol = 0.5, //объем в микронах кубических,
+		dstep = 1E-4, //параметр решетки в микронах
+		kv = 1E-12, //перевод к сантиметрам кубическим и к сантиметрам,
+		md; //модуль магнитного момента
+//векторы
+
+struct vect {
+	double x, y, z;
+};
+
+//класс частицы
+
+class Part {
+public:
+	int x, y, z; //координаты, для удобства в относительных величинах
+	double absX, absY, absZ; //абсолютные координаты
+	vect axis; //ось частицы
+	vect m; //магнитный момент
+	vect interaction; //поле взаимодействия
+	double intMod; //модуль (длина) поля взаимодействия
+
+	Part();
+	/**
+	 * Считает взаимодействия между этой частицей и остальным массивом частиц
+     * @param
+     */
+	void calcInteraction(vector < vector < vector < Part > > >*);
+};
+
+/**
+ * Скалярное произведение двух векторов
+ * @param a первый вектор
+ * @param b второй вектор
+ * @return
+ */
+double scalar(vect a, vect b) {
+	return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+/**
+ * Находит расстояние между двумя векторами
+ * @param a
+ * @param b
+ * @return
+ */
+double space(vect a, vect b) {
+	return sqrt(
+			(a.x - b.x)*(a.x - b.x) +
+			(a.y - b.y)*(a.y - b.y) +
+			(a.z - b.z)*(a.z - b.z)
+			);
+}
+
+/**
+ * длина вектора
+ * @return
+ */
+double length(vect a){
+	return sqrt(
+			a.x * a.x +
+			a.y * a.y +
+			a.z * a.z
+			);
+}
+
 int main() {
+	Part* temp; //временныый элемент частицы, для ускорения кода
 
-	int f = 6, N = f*f; //number of particles
-	double *x0 = new double [N + 1]; //koordinaty chastic x,y,z
-	double *y0 = new double [N + 1];
-	double *z0 = new double [N + 1];
-
-	double *cosX = new double [N + 1]; //косинусы chastic x,y,z
-	double *cosY = new double [N + 1];
-	double *cosZ = new double [N + 1];
-
-	double *mX = new double [N + 1]; //magnitnie momenty
-	double *mY = new double [N + 1];
-	double *mZ = new double [N + 1];
-
-	double *hrrx = new double [N + 1]; //поля взаимодействия х
-	double *hrry = new double [N + 1]; //поля взаимодействия y
-	double *hrrz = new double [N + 1]; //поля взаимодействия z
-
-	double *hrm = new double [N + 1]; //модули полей взаимодействия
-	double I0s = 500, hc = 50, vol = 0.5, dstep = 1E-4, kv = 1E-12, md;
-	//намагниченность насыщения в гаусс, критическое поле в эрстедах, объем в микронах кубических, параметр решетки в микронах
-	//перевод к сантиметрам кубическим и к сантиметрам, модуль магнитного момента
-
-
-
-	for (int l = 1; l <= f; l++)
-		for (int i = 1; i <= f; i++) {
-			x0[i + (l - 1) * f] = (i - 1) * dstep;
-			y0[i + (l - 1) * f] = 0 * dstep;
-			z0[i + (l - 1) * f] = (l - 1) * dstep;
+	//инициализируем 3-х мерный массив parts
+	vector < vector < vector < Part > > > parts;
+	parts.resize(N);
+	for (int i = 0; i < N; i++) {
+		parts[i].resize(N);
+		for (int j = 0; j < N; j++) {
+			parts[i][j].resize(N);
 		}
+	}
+
+	//задание начальных значений
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				temp = &parts[i][j][k];
+				//координаты частицы (для удобства)
+				temp->x = i;
+				temp->y = j;
+				temp->z = k;
+
+				//абсолютные координаты частицы
+				temp->absX = temp->x * dstep;
+				temp->absY = temp->y * dstep;
+				temp->absZ = temp->x * dstep;
+
+				//магн. моменты
+				temp->m.x = kv * vol * temp->axis.x * I0s;
+				temp->m.y = kv * vol * temp->axis.y * I0s;
+				temp->m.z = kv * vol * temp->axis.z * I0s;
+			}
+		}
+	}
+
+	//выполнение рассчетов
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				temp = &parts[i][j][k];
+				temp->calcInteraction(&parts);
+				temp->intMod = length(temp->interaction);
+			}
+		}
+	}
+
+	//так как каждый элемент решетки имеет электрон - то доступ к каждому электрону с координатами {x,y,z}
+	//будет происходить по parts[x][y][z]
+	cout << parts[0][0][0].axis.x << " " << parts[0][0][0].axis.z;
+	/* ???
+		for (int i = 0; i < f; i++)
+			for (int j = 1; j <= f; j++) {
+				x0[j + i * f] = (j - 1) * dstep;
+				y0[j + i * f] = 0 * dstep;
+				z0[j + i * f] = (j - 1) * dstep;
+			}
+	 *
 
 
 	for (int i = 1; i <= f * f; i++) {
-		cosX[i] = 0.; // если все параллельны
-		cosY[i] = 0.;
-		cosZ[i] = 1.; // все по оси z и по полю
-
-		mX[i] = kv * vol * cosX[i] * I0s;
-		mY[i] = kv * vol * cosY[i] * I0s;
-		mZ[i] = kv * vol * cosZ[i] * I0s;
-
 		cout << "x0[" << i << "]=" << x0[i] << " y0[" << i << "]=" << y0[i] << " z0[" << i << "]=" << z0[i] << endl;
 		cout << "mx[" << i << "]=" << mX[i] << " my[" << i << "]=" << mY[i] << " mz[" << i << "]=" << mZ[i] << endl << endl;
 	}
 
 	md = kv * vol*I0s;
 	cout << " |m|=" << md << " |M|=" << f * f * md << endl;
-
+	 */
 
 	system("pause");
+}
+
+Part::Part() {
+	//оси частиц направлены вдоль оси Z
+	this->axis.x = .0;
+	this->axis.y = .0;
+	this->axis.z = 1.;
+
+	//взаимодействие частиц пока ноль
+	this->interaction.x = this->interaction.y = this->interaction.z = this->intMod = 0;
+}
+
+void Part::calcInteraction(vector < vector < vector < Part > > >* parts) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				if (i != this->x && j != this->y && k != this->z) { //не считать взаимодействие частицы на себя
+					this->interaction.x += 1;
+					this->interaction.y += 1;
+					this->interaction.z += 1;
+				}
+			}
+		}
+	}
 }
