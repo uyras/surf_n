17bd654d8b0292e8b6f95fbca506700790f9a596
Отморозок <@uyras-pc>
2012-12-05 01:50:15 +1100

    рабочая версия, считаем закон распределения

--- distributionLaw.h
+++ distributionLaw.h
@@ -11,10 +11,12 @@
 #include <cmath>
 
 using namespace std;
-template <typename arrType> class distributionLaw {
+//template <typename arrType> class distributionLaw {
+class distributionLaw {
 public:
-	distributionLaw(int count, arrType array);
-	int* calculate();
+	distributionLaw();
+	//distributionLaw(int count, arrType array);
+	int* calculate(int count);
 	double* getLables(int count);
 	vector<double> values; //значения, для которых рассчитан закон распределения
 	int* counts; //массив количества попаданий значения в интервал
